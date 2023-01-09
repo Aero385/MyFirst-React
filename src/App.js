c1d78c891ba36ab89934ddae9a5d605bc5c170d1
Stanislav Prokofjev <Prokostas@gmail.com>
2023-01-09 23:47:30 +0200

    add hooks/ animation/ add modul list

--- src/App.js
+++ src/App.js
@@ -1,74 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
+import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyInput from './components/UI/input/MyInput';
-import MySelect from './components/UI/select/MySelect';
+import MyButton from './components/UI/button/MyButton';
+import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 // import ClassCounter from './components/ClassCounter';
 // import Counter from './components/Counter';
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'aa', body: 'ff'},
-    {id: 2, title: 'dd', body: 'jj'},
-    {id: 3, title: 'cc', body: 'dd'},
-  ]) 
-
-  const [selectedSort, setSelectedSort] = useState('');
-
-  const [seacrhQuery, setSearchQuery] = useState('');
-
-  function getSortedPosts() {
-    console.log('WORKED')
-    if(selectedSort){
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort])) ;
-    } else {
-      return posts;
-    }
-  }
-
-  const sortedPosts = getSortedPosts();
+  const [posts, setPosts] = useState([]) 
+  const [filter, setFilter] = useState({sort:'', query:''})
+  const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-  }
 
   return (
     <div className='App'>
-      <PostForm create={createPost}/>
-      <hr style={{margin:'15px 0'}}></hr>
-      <div>
-        <MyInput
-          placeholder='Search...'
-          value={seacrhQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue='Sort by'
-          option ={[
-            {value: 'title', name: 'By name'},
-            {value: 'body', name: 'By description'},
-          ]}
-        />
-      </div>
-      {posts.length !== 0
-        ? <PostList remove={removePost} posts={sortedPosts} title='list of posts 1'/>
-        : <h1 style={{textAlign: 'center'}}>
-            There is no posts
-          </h1>
-      }
+      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+        Create user
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
      
+      <hr style={{margin:'15px 0'}}></hr>
+      <PostFilter 
+        filter={filter} 
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='list of posts 1'/>
     </div>
   )
 }
