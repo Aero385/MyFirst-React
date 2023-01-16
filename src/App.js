ff1746dbeb195f4d1c9760acaed24fd438490a4b
Stanislav Prokofjev <Prokostas@gmail.com>
2023-01-16 23:47:55 +0200

    add login/logout without backend and new hook useContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 import {
   BrowserRouter,
 } from "react-router-dom";
 import Navbar from './components/UI/Navbar/Navbar';
 import AppRouter from './components/AppRouter';
+import { AuthContext } from './contex';
 
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if(localStorage.getItem('auth')){
+      setIsAuth(true)
+    }
+    setLoading(false)
+  }, [])
+
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <AppRouter/>
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+        isAuth,
+        setIsAuth,
+        isLoading,
+      }}>
+      <BrowserRouter>
+        <Navbar/>
+        <AppRouter/>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
